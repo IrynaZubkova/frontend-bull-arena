7a538cfbf29fa0ad778f5d86582e2da6fe18745d
IrynaZubkova <166387183+IrynaZubkova@users.noreply.github.com>
2024-09-24 21:38:09 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import './App.css'
 import ArenaWithBull from './components/ArenaWithBull'
 import { Matador } from './components/Matador'
+import OldMatador from './components/OldMatador'; 
+import React, { useState } from 'react';
 
 function App() {
+
+    const [matadorPosition, setMatadorPosition] = useState(0);
+    const [applause, setApplause] = useState(0);
+  
+    // This function is just for demonstration. You should replace it with your actual logic.
+    const handleApplauseChange = (newApplause: number) => {
+      setApplause(newApplause);
+  };
   return (
     <div className="App">
       <ArenaWithBull
-        matador={<Matador />} />
-    </div>
+        matador={
+            <OldMatador
+              applause={applause}
+              setMatarodPosition={setMatadorPosition}
+              matadorPosition={matadorPosition}
+            />
+          }
+        />
+      </div>
   )
 }
 
